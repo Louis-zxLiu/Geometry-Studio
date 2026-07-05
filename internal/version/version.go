c1ad251cd1da819e4e81984dc8b44d9b395073e0
Wing900 <2249381074@qq.com>
2026-07-05 23:05:09 +0800

    chore: bump version to 0.0.4.21

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,12 +2,12 @@ package version
 
 import "strings"
 
-var appVersion = "0.0.4.2"
+var appVersion = "0.0.4.21"
 
 func Current() string {
 	value := strings.TrimSpace(appVersion)
 	if value == "" {
-		return "0.0.4.2"
+		return "0.0.4.21"
 	}
 
 	return value
